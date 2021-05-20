b93ddbf8bd560ad816f318eca280f86ffa9f4a21
karishmas6 <carishmashukla28@gmail.com>
2021-05-21 02:45:32 +0530

    Fixed small bugs

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -12,7 +12,7 @@ import Education from './Education';
 const Dashboard = ({ getCurrentProfile, deleteAccount, auth:{ user }, profile: { profile, loading } }) => {
     useEffect(() => {
         getCurrentProfile();
-    }, [getCurrentProfile]);
+    });
 
     return loading && profile === null ? <Spinner /> : <Fragment>
         <h1 className="large text-primary">Dashboard</h1>
@@ -26,8 +26,7 @@ const Dashboard = ({ getCurrentProfile, deleteAccount, auth:{ user }, profile: {
           <Education education={profile.education} />
           <div className="my-2">
               <button className="btn btn-danger" onClick={() => deleteAccount()}>
-                  <i className="fas fa-user-minus"></i>
-                  Delete My Account
+                  <i className="fas fa-user-minus" /> Delete Account
               </button>
           </div>
         </Fragment>
