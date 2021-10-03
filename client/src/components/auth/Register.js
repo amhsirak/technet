c206fefe312487c1d18ddd099c42bcb4520ea6f6
Aman Sagar <amansagar262@gmail.com>
2021-10-03 18:03:02 +0530

    register page changes

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -11,77 +11,97 @@ const Register = ({ setAlert,register, isAuthenticated}) => {
         name: '',
         email: '',
         password: '',
-        password2: ''
+        password2: '',
+        error: ''
     });
 
-    const { name,email,password,password2} = formData;
+    const { name,email,password,password2,error} = formData;
 
-    const handleChange = e => setFormData({ ...formData, [e.target.name]: e.target.value});
+    const handleChange = e => setFormData((prevState)=>({ ...prevState, error: '', [e.target.name]: e.target.value}));
 
     const onSubmit = async e => {
-        e.preventDefault();
-        if(password !== password2) {
-            setAlert('Passwords do not match', 'danger');
-        } else {
-            register({ name,email,password });
+      e.preventDefault();
+      if (!(password.length > 5)) {
+        setFormData(prevState=>({...prevState, error: 'password should be atlease 6 character long'}))
+      }
+      else if (password !== password2) {        
+        setAlert('Passwords do not match', 'danger');        
+      }
+      else {
+        register({ name,email,password });
+      }      
     }
-}
+
 // If user is registered
 if(isAuthenticated){
   return <Redirect to="/dashboard" />
 };
 
     return( 
-    <Fragment>
+    <>
         <h1 className="large text-primary">Sign Up</h1>
         <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
         <form className="form" onSubmit={onSubmit}>
           <div className="form-group">
-            <input type="text" 
-            placeholder="Full Name" 
-            name="name" 
-            // onChange={e => handleChange(e)}
-            value={name} 
-            onChange = {handleChange} 
-            // required 
+            <div className="input-container">          
+              <i className="fas fa-user"></i>
+              <input            
+              type="text"
+              placeholder="Full Name" 
+              name="name"
+              required
+              value={name} 
+              onChange = {handleChange} 
             />
+          </div>  
+           
           </div>
           <div className="form-group">
+            <div className="input-container">
+            <i class="fas fa-envelope"></i>
             <input type="email" 
-            placeholder="Email Address" 
-            name="email"
-            value={email} 
-            onChange = {handleChange} 
-            // required 
-            />
+              placeholder="Email Address" 
+              name="email"
+              value={email} 
+              onChange={handleChange}
+              required
+             
+              />
+            </div>
             <small className="form-text"
               >This site uses Gravatar. If you want a profile image, use a
               Gravatar email</small
             >
           </div>
           <div className="form-group">
+            <div className="input-container">
+            <i class="fas fa-lock"></i>              
             <input
               type="password"
               placeholder="Password"
               name="password"
-              value={password} 
+              value={password}
+              required
               onChange = {handleChange} 
-              // minLength="6"
-              // required
-               />
-              <small className="form-text"
-              >Password must contain atleast 6 characters</small>
+            />
+            </div>
+              <span className="form-text" style={{color: 'red'}}>            
+              {error && error}
+            </span>
           </div>
           <div className="form-group">
+            <div className="input-container">
+            <i class="fas fa-lock"></i>           
             <input
+              required
               type="password"
               placeholder="Confirm Password"
               name="password2"
               value={password2} 
               onChange = {handleChange} 
-              // minLength="6"
-              // required
+             
             />
+            </div>
           </div>
           <input type="submit" className="btn btn-primary" value="Register" />
         </form>
@@ -91,7 +111,7 @@ if(isAuthenticated){
               Sign In
           </Link>
         </p>
-        </Fragment>
+        </>
     );
   };
 
